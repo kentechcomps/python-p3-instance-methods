38f2aeca6d2416cd8bce6f8a6fd9ad47e5778af3
user <mutukukennedy34@gmail.com>
2023-08-21 12:32:19 +0300

    instance

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,12 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello world!")
+
+    def walk(self):
+        print("The person is walking")
+
+Mwalyo = Person()
+Mwalyo.talk()
+Mwalyo.walk()
